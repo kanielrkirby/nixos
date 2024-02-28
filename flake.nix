82b1332c91422242495633af6e1f326299d7dc66
Kaniel Kirby <piratey7007@runbox.com>
2024-02-27 22:19:23 -0600

    update

--- flake.nix
+++ flake.nix
@@ -1,8 +1,13 @@
 {
   description = "My NixOS configuration";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.hyprland.url = "github:hyprwm/Hyprland";
+  inputs = {
+      hyprland.url = "github:hyprwm/Hyprland";
+      nixpkgs.url = "github:NixOS/nixpkgs/d8e0944e6d2ce0f326040e654c07a410e2617d47";
+      # Switch to unstable when the problem above is fixed
+      #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
 
   outputs = {nixpkgs, ...} @ inputs: {
     nixosConfigurations = {
