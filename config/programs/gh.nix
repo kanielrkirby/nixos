99f53bb80b23228548a94b4e9cabcac93de72bcd
Kaniel Kirby <piratey7007@runbox.com>
2024-04-06 18:51:55 -0500

    Update

--- config/programs/gh.nix
+++ config/programs/gh.nix
@@ -2,7 +2,7 @@
 
 {
   home-manager.users."${username}" = {
-    home.packages = with pkgs; [ gh-eco gh-f gh-s ];
+    home.packages = with pkgs; [ gh-eco gh-f ];
     programs.gh-dash = {
       enable = true;
       settings = {
