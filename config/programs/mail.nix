72898eff62a5236098fe5ae046bbb7f8dab3193c
Kaniel Kirby <piratey7007@runbox.com>
2024-04-06 20:03:37 -0500

    Update

--- config/programs/mail.nix
+++ config/programs/mail.nix
@@ -16,44 +16,44 @@
     export POP_SMTP_USERNAME="$(cat "${config.sops.secrets."runbox.com/username".path}")"
     export POP_SMTP_PASSWORD="$(cat "${config.sops.secrets."runbox.com/app/pop/password".path}")"
     '';
-  };
 
-  programs.mbsync = {
-    enable = true;
-    accounts = {
-      personal = {
-        create = true;
-        enable = true;
-        user = "maxisacat@runbox.com";
-        passCmd = "cat ${config.sops.secrets."runbox.com/app/mbsync/password".path}";
-        host = "mail.runbox.com";
+    programs.mbsync = {
+      enable = true;
+      accounts = {
+        personal = {
+          create = true;
+          enable = true;
+          user = "maxisacat@runbox.com";
+          passCmd = "cat ${config.sops.secrets."runbox.com/app/mbsync/password".path}";
+          host = "mail.runbox.com";
 
-        Patterns = ["Inbox" "Spam" "Sent" "Trash"];
-        # subFolders = "Verbatim";
-        path = "~/.mail/";
-        inbox = "~/.mail/Inbox";
+          Patterns = ["Inbox" "Spam" "Sent" "Trash"];
+          # subFolders = "Verbatim";
+          path = "~/.mail/";
+          inbox = "~/.mail/Inbox";
 
-        channels = {
-          sync-personal-inbox = {
-            slave = ":personal-local:Inbox";
-            master = ":personal-remote:'Inbox'";
+          channels = {
+            sync-personal-inbox = {
+              slave = ":personal-local:Inbox";
+              master = ":personal-remote:'Inbox'";
+            };
+            sync-personal-archive = {
+              slave = ":personal-local:Spam";
+              master = ":personal-remote:'Spam'";
+            };
+            sync-personal-sent = {
+              slave = ":personal-local:Sent";
+              master = ":personal-remote:'Sent'";
+            };
+            sync-personal-trash = {
+              slave = ":personal-local:Trash";
+              master = ":personal-remote:'Trash'";
+            };
           };
-          sync-personal-archive = {
-            slave = ":personal-local:Spam";
-            master = ":personal-remote:'Spam'";
-          };
-          sync-personal-sent = {
-            slave = ":personal-local:Sent";
-            master = ":personal-remote:'Sent'";
-          };
-          sync-personal-trash = {
-            slave = ":personal-local:Trash";
-            master = ":personal-remote:'Trash'";
-          };
-        };
-        groups = {
-          Personal = {
-            channels = ["sync-personal-inbox" "sync-personal-spam" "sync-personal-sent" "sync-personal-trash"];
+          groups = {
+            Personal = {
+              channels = ["sync-personal-inbox" "sync-personal-spam" "sync-personal-sent" "sync-personal-trash"];
+            };
           };
         };
       };
