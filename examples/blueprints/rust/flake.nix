35663a74d79aef6bda10c065b3e3b77062332a2c
Kaniel Kirby <piratey7007@runbox.com>
2024-05-19 09:35:48 -0500

    misc

--- examples/blueprints/rust/flake.nix
+++ examples/blueprints/rust/flake.nix
@@ -22,29 +22,59 @@
   in
   {
     devShell = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        gnumake
-        gcc
-        curl
-        wget
-        file
-        pkg-config
-        dbus
-        openssl
-        glib
-        trunk
-        evcxr
-        clippy
-        bacon
-        (with fenix.packages.${system}; combine [
-          latest.toolchain
-        ])
-      ];
+        buildInputs = with pkgs; [
+          openssl
+          pkg-config
+          cacert
+          cargo-make
+          cargo-leptos
+          cargo-whatfeatures
+          sass
+          tailwindcss
+          mold
+          sccache
+          evcxr
+          bacon
+          trunk
+          (with fenix.packages.${system}; combine [
+            latest.toolchain
+            targets.wasm32-unknown-unknown.latest.toolchain
+          ])
+        ];
 
-      shellHook =
-        ''
-          echo "$(rustc --version)";
-          echo "Environment ready with Rust."; 
+        shellHook = ''
+          echo "$(
+            echo
+            echo "Installed Targets:"
+            echo
+            echo "$(${pkgs.rustup}/bin/rustup target list --installed | sed 's/^.*/  \0/')"
+          )"
+          echo "$(
+            cmds=(
+              "cargo --version | sed 's/ (.*$//'"
+              "trunk --version"
+              "evcxr --version"
+              "bacon --version"
+              "sccache --version"
+              "cargo leptos --version"
+              "cargo make --version"
+              "cargo whatfeatures --version"
+              'echo "pkg-config $(pkg-config --version)"'
+              "mold --version | sed 's/ (.*$//'"
+              "sass --version"
+              "tailwindcss --help | head -n 2 | tail -n 1"
+              "openssl version | sed 's/\([^ ]* [^ ]*\).*/\1/';"
+            );
+            echo
+            echo "Environment Packages:"
+            echo
+            for cmd in $cmds; do
+              eval "$cmd";
+            done
+            echo "$(
+            )" | sed 's/^.*/  \0/'
+          )"
+          echo
         '';
       };
     });
