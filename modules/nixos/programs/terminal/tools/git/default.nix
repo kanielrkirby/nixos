12a323515316a305aab81291bdf77959797a26cb
Kaniel Kirby <pirate7007@runbox.com>
2024-06-27 17:11:28 -0500

    rework: `git` aliases moved to functions

--- modules/nixos/programs/terminal/tools/git/default.nix
+++ modules/nixos/programs/terminal/tools/git/default.nix
@@ -20,6 +20,69 @@ in {
       userEmail = "pirate7007@runbox.com";
       userName = "Kaniel Kirby";
       extraConfig.safe.directory = ["/etc/nixos"];
+      zsh.initExtra = ''
+ga() {
+  if [ -z "$1" ]; then
+    git add .
+  else
+    git add "$@"
+  fi
+}
+
+gc() {
+  if [ -z "$1" ]; then
+    git commit
+  else
+    git commit -m "$*"
+  fi
+}
+
+gp() {
+  git push "$@"
+}
+
+gac() {
+  ga
+  gc "$@"
+}
+
+gcp() {
+  gc "$@"
+  gp
+}
+
+gacp() {
+  ga
+  gcp "$@"
+}
+
+sga() {
+  sudo -E ga "$@"
+}
+
+sgc() {
+  sudo -E gc "$@"
+}
+
+sgp() {
+  sudo -E gp "$@"
+}
+
+sgac() {
+  sga
+  sgc "$@"
+}
+
+sgcp() {
+  sgc "$@"
+  sgp
+}
+
+sgacp() {
+  sga
+  sgcp "$@"
+}
+      '';
     };
   };
 }
