6ac7c44d93f95d8dcf20bf7ee6151cb1625045e1
Kaniel Kirby <pirate7007@runbox.com>
2024-06-27 17:11:28 -0500

    Added git convenience aliases

--- modules/nixos/programs/terminal/tools/git/default.nix
+++ modules/nixos/programs/terminal/tools/git/default.nix
@@ -20,6 +20,21 @@ in {
       userEmail = "pirate7007@runbox.com";
       userName = "Kaniel Kirby";
       extraConfig.safe.directory = ["/etc/nixos"];
+      zsh.initExtra = ''
+        alias ga='if [ -z "$@" ]; then git add .; else git add "$@"; fi'
+        alias gc='if [ -z "$@" ]; then git commit; else git commit -m "$@" fi'
+        alias gp='git push "$@"'
+        alias gac='if [ -z "$@" ]; then git add . && git commit && git push; else git add . && git commit -m "$@"'
+        alias gacp='if [ -z "$@" ]; then git add . && git commit && git push; else git add . && git commit -m "$@" && git push'
+        alias gcp='if [ -z "$@" ]; then git commit && git push; else git commit -m "$@" && git push'
+
+        alias sga='if [ -z "$@" ]; then sudo -E git add .; else sudo -E git add "$@"; fi'
+        alias sgc='if [ -z "$@" ]; then sudo -E git commit; else sudo -E git commit -m "$@" fi'
+        alias sgp='sudo -E git push "$@"'
+        alias sgac='if [ -z "$@" ]; then sudo -E git add . && sudo -E git commit && sudo -E git push; else sudo -E git add . && sudo -E git commit -m "$@"'
+        alias sgacp='if [ -z "$@" ]; then sudo -E git add . && sudo -E git commit && sudo -E git push; else sudo -E git add . && sudo -E git commit -m "$@" && sudo -E git push'
+        alias sgcp='if [ -z "$@" ]; then sudo -E git commit && sudo -E git push; else sudo -E git commit -m "$@" && sudo -E git push'
+      '';
     };
   };
 }
