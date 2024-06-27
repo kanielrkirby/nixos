2185bf90c39f6477cb3dbbfc21c799fdfe35a90c
Kaniel Kirby <pirate7007@runbox.com>
2024-06-27 17:40:23 -0500

    Made aliases functions for better git stuff

--- modules/nixos/programs/terminal/tools/git/default.nix
+++ modules/nixos/programs/terminal/tools/git/default.nix
@@ -21,19 +21,67 @@ in {
       userName = "Kaniel Kirby";
       extraConfig.safe.directory = ["/etc/nixos"];
       zsh.initExtra = ''
-        alias ga='if [ -z "$@" ]; then git add .; else git add "$@"; fi'
-        alias gc='if [ -z "$@" ]; then git commit; else git commit -m "$@"; fi'
-        alias gp='git push "$@";'
-        alias gac='if [ -z "$@" ]; then git add . && git commit && git push; else git add . && git commit -m "$@"; fi'
-        alias gacp='if [ -z "$@" ]; then git add . && git commit && git push; else git add . && git commit -m "$@" && git push; fi'
-        alias gcp='if [ -z "$@" ]; then git commit && git push; else git commit -m "$@" && git push; fi'
+ga() {
+  if [ -z "$1" ]; then
+    git add .
+  else
+    git add "$@"
+  fi
+}
 
-        alias sga='if [ -z "$@" ]; then sudo -E git add .; else sudo -E git add "$@"; fi'
-        alias sgc='if [ -z "$@" ]; then sudo -E git commit; else sudo -E git commit -m "$@"; fi'
-        alias sgp='sudo -E git push "$@";'
-        alias sgac='if [ -z "$@" ]; then sudo -E git add . && sudo -E git commit && sudo -E git push; else sudo -E git add . && sudo -E git commit -m "$@"; fi'
-        alias sgacp='if [ -z "$@" ]; then sudo -E git add . && sudo -E git commit && sudo -E git push; else sudo -E git add . && sudo -E git commit -m "$@" && sudo -E git push; fi'
-        alias sgcp='if [ -z "$@" ]; then sudo -E git commit && sudo -E git push; else sudo -E git commit -m "$@" && sudo -E git push; fi'
+gc() {
+  if [ -z "$1" ]; then
+    git commit
+  else
+    git commit -m "$*"
+  fi
+}
+
+gp() {
+  git push "$@"
+}
+
+gac() {
+  ga
+  gc "$@"
+}
+
+gcp() {
+  gc "$@"
+  gp
+}
+
+gacp() {
+  ga
+  gcp "$@"
+}
+
+sga() {
+  sudo -E ga "$@"
+}
+
+sgc() {
+  sudo -E gc "$@"
+}
+
+sgp() {
+  sudo -E gp "$@"
+}
+
+sgac() {
+  sga
+  sgc "$@"
+}
+
+sgcp() {
+  sgc "$@"
+  sgp
+}
+
+sgacp() {
+  sga
+  sgcp "$@"
+}
       '';
     };
   };
