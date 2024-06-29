05a9d367cc63fae253a0028bcb69b4e014af3dd1
Kaniel Kirby <pirate7007@runbox.com>
2024-06-29 07:13:20 -0500

    add module: `waybar`

---
+++ modules/nixos/programs/wayland/waybar/default.nix
@@ -0,0 +1,24 @@
+{
+  pkgs,
+  config,
+  lib,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+
+  cfg = config.${namespace}.programs.wayland.waybar;
+in {
+  options.${namespace}.programs.wayland.waybar = {
+    enable = mkBoolOpt false "Whether or not to enable waybar.";
+  };
+
+  config = mkIf (cfg.enable && config.${namespace}.user.enable) {
+    home-manager.users.${config.${namespace}.user.name} = {
+      programs.waybar = {
+        enable = true;
+      };
+    };
+  };
+}
