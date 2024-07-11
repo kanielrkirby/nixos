a3a550a3ae87808a6dcf1120e4bf893cf8ae6cb8
Kaniel Kirby <pirate7007@runbox.com>
2024-07-11 13:09:00 -0500

    rework: update flake, move under `gearshift`

--- modules/nixos/suites/dev/default.nix
+++ modules/nixos/suites/dev/default.nix
@@ -11,23 +11,28 @@ with lib;
 
   config = mkMerge [
     (mkIf config.gearshift.suite.dev.enable {
-      gearshift.alacritty.enable = true;
-      gearshift.bat.enable = true;
-      gearshift.browser.have-all = true;
-      gearshift.btop.enable = true;
-      gearshift.eza.enable = true;
-      gearshift.fzf.enable = true;
-      gearshift.gh.enable = true;
-      gearshift.git.enable = true;
-      gearshift.less.enable = true;
-      gearshift.mods.enable = true;
-      gearshift.neovim.enable = true;
-      # config.gearshift.ollama.enable = true;
-      gearshift.starship.enable = true;
-      gearshift.tealdeer.enable = true;
-      gearshift.vscode.enable = true;
-      gearshift.zoxide.enable = true;
-      gearshift.zsh.enable = true;
+      gearshift = {
+        ripgrep.enable = true;
+        yarn.enable = true;
+        alacritty.enable = true;
+        bat.enable = true;
+        browser.have-all = true;
+        btop.enable = true;
+        eza.enable = true;
+        fzf.enable = true;
+        feh.enable = true;
+        gh.enable = true;
+        git.enable = true;
+        less.enable = true;
+        lazysql.enable = true;
+        mods.enable = true;
+        neovim.enable = true;
+        starship.enable = true;
+        tealdeer.enable = true;
+        vscode.enable = true;
+        zoxide.enable = true;
+        zsh.enable = true;
+      };
     })
   ];
 }
--- modules/nixos/suites/home/default.nix
+++ modules/nixos/suites/home/default.nix
@@ -12,12 +12,28 @@ with lib;
   config = mkMerge [
     (mkIf config.gearshift.suite.home.enable {
       gearshift = {
+        brightnessctl.enable = true;
+        foliate.enable = true;
+        dunst.enable = true;
+        grim.enable = true;
+        fw-ectool.enable = true;
+        libnotify.enable = true;
+        nom.enable = true;
+        playerctl.enable = true;
+        slurp.enable = true;
+        waybar.enable = true;
+        wf-recorder.enable = true;
+        wl-clipboard.enable = true;
+        youtube-tui.enable = true;
         browser.brave.enable = true;
         localsend.enable = true;
         mail.enable = true;
         signal.enable = true;
+        smassh.enable = true;
         transmission.enable = true;
         fuzzel.enable = true;
+        warpd.enable = true;
+        ydotool.enable = true;
       };
     })
   ];
