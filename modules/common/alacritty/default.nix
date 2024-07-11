25ab9f48770d37e59efc2b6a05c5c5803f9de7aa
Kaniel Kirby <pirate7007@runbox.com>
2024-07-11 13:15:09 -0500

    fix: general formatting

--- modules/common/alacritty/default.nix
+++ modules/common/alacritty/default.nix
@@ -10,12 +10,13 @@
       programs.kitty = {
         enable = true;
         extraConfig = ''
-          background_opacity 0.7
-          background         #000000
-          font_family        MonaspiceNe NF
-          italic_font        MonaspiceRn NF
-          font_size          12
-          enable_audio_bell  no
+          confirm_os_window_close 0
+          background_opacity      0.7
+          background              #000000
+          font_family             MonaspiceNe NF
+          italic_font             MonaspiceRn NF
+          font_size               12
+          enable_audio_bell       no
         '';
       };
     };
