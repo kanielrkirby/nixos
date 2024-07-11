fbdc86cd4faf92f6ffa7c21461cac339cadecd74
Kaniel Kirby <pirate7007@runbox.com>
2024-07-11 13:10:15 -0500

    rework: extract scripts into modules

--- modules/nixos/base/default.nix
+++ modules/nixos/base/default.nix
@@ -47,122 +47,24 @@ with lib;
 
     system.nixos.label = "";
 
-
     home-manager = {
       useGlobalPkgs = true;
 
       users."${config.gearshift.username}" = {
-        xdg.configFile."nom/config.yml".text = ''
-          feeds:
-            - url: https://nixos.org/blog/feed.xml
-              name: NixOS Official Blog
-            - url: https://old.reddit.com/r/NixOS/.rss
-              name: NixOS Reddit
-            - url: https://github.com/nix-community/awesome-nix/commits.atom
-              name: NixOS Awesome GitHub
-        '';
-  
-        programs.feh.enable = true;
-  
-        programs.zsh.initExtra = ''
-        '';
-
-        services.dunst.enable = true;
-
         home = {
           stateVersion = config.gearshift.version.home;
-          packages = with pkgs; [
-            libnotify
-            hyprshade
-            fw-ectool
-            httpie
-            grim
-            slurp
-            ripgrep
-            brightnessctl
-            playerctl
-            foliate
-            yarn
-            youtube-tui
-            nom
-            wf-recorder
-            wl-clipboard
-            waybar
-          ];
         };
       };
     };
 
     environment.systemPackages = with pkgs; [
       git
-      (writeShellScriptBin "n" ''
-        __args="$@"
-        __cmd="$1"
-        nix shell "nixpkgs#$__cmd" --command $__cmd ''${__args#* }
-      '')
-      (writeShellScriptBin "up" ''
-      __current_dir="$(pwd)"
-      __cleanup() {
-        __v="$1"
-        if [[ -z "$__v" ]]; then
-          __v=0
-        fi
-        unset __current_dir
-        unset __cmd
-        unset __message
-        unset __git
-        unset __nopush
-        unset NIXOS_LABEL
-        cd "$__current_dir"
-        exit "$__v"
-      }
-      __message=""
-      __nopush=""
-      __git=""
-      __cmd="test"
-
-      for __arg in "$@"; do
-        case $__arg in
-          -g) __git="1" ;;
-          -p) __nopush="1" ;;
-          -b) __cmd="boot" ;;
-          -s) __cmd="switch" ;;
-          -t) __cmd="test" ;;
-          *) 
-            if [[ -z "$__message" ]]; then
-              __message="$__arg"
-            else
-              echo "Error: Unexpected argument: $__arg"
-              __cleanup 1
-            fi
-          ;;
-        esac
-      done
-
-      cd /etc/nixos
-
-      if [[ "$__git" ]]; then
-        if [[ -z "$__message" ]]; then
-          echo "Must specify commit message when using Git."
-          __cleanup
-          exit 1
-        fi
-        sudo -E git add . 
-        sudo -E git commit -m "$__message" 
-        if [[ -z "$__nopush" ]]; then
-          sudo -E git push 
-        fi
-      fi
-
-      export NIXOS_LABEL="$__message"
-
-      sudo -E nixos-rebuild "$__cmd" --flake /etc/nixos#eclipse
-      __cleanup
-      '')
-      (callPackage ../../derivations/goread.nix {})
-      (callPackage ../../derivations/lazysql.nix {})
-      (callPackage ../../derivations/smassh.nix {})
     ];
+
+    gearshift.scripts = {
+      ns.enable = true;
+      up.enable = true;
+    };
 
     system.stateVersion = config.gearshift.version.main;
   };
